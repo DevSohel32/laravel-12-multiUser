38eb67af67fba5d3f654ca505c1e85d5140a813e
DevSohel32 <devsohel@example.com>
2026-01-18 20:58:53 +0600

    Update admin login and forgot password forms, update routes

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,19 @@ use App\Http\Controllers\Admin\AdminController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FrontendController;
 
-Route::get('/',[FrontendController::class,'index'])->name('home');
-Route::get('/about',[FrontendController::class,'about'])->name('about');
-Route::get('/about',[FrontendController::class,'about'])->name('about');
+Route::get('/', [FrontendController::class, 'index'])->name('home');
+Route::get('/about', [FrontendController::class, 'about'])->name('about');
+Route::get('/about', [FrontendController::class, 'about'])->name('about');
 
 
-Route::prefix('admin')->group(function(){
-    Route::get('/login', [AdminController::class, 'admin_login'])->name('admin_login');
+Route::middleware('auth:admin')->prefix('admin')->group(function () {
+    Route::get('/dashboard', [AdminController::class, 'admin_dashboard'])->name('admin_dashboard');
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('/login', [AdminController::class, 'login'])->name('admin_login');
     Route::post('/login/submit', [AdminController::class, 'admin_login_submit'])->name('admin_login_submit');
     Route::get('/forgot/password', [AdminController::class, 'admin_forgot_password'])->name('admin_forgot_password');
     Route::post('/forgot/password/submit', [AdminController::class, 'admin_forgot_password_submit'])->name('admin_forgot_password_submit');
+    Route::get('/logout', [AdminController::class, 'logout'])->name('admin_logout');
 });
